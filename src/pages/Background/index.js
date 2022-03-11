67bd08159ecd6c19c5e51f2dc39d32f702baca13
KristofferTroncoso <troncosokristoffer@gmail.com>
2022-03-11 00:37:22 -0500

    Fix broken extension on new installed or update

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -63,3 +63,13 @@ chrome.runtime.onInstalled.addListener((details) => {
       chrome.storage.sync.set(defaults);
   }
 });
+
+
+chrome.tabs.query({url: 'https://music.youtube.com/*'}, (tabs) => {
+  for (let tab of tabs) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['contentScript.bundle.js'],
+    });
+  }
+});
