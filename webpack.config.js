27809a548a8b398799e6b38d03d6edeef95ab887
KristofferTroncoso <troncosokristoffer@gmail.com>
2022-05-20 00:25:02 -0400

    Remove content.styles.css

--- webpack.config.js
+++ webpack.config.js
@@ -137,15 +137,6 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/pages/Content/content.styles.css',
-          to: path.join(__dirname, 'build'),
-          force: true,
-        },
-      ],
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
           from: 'src/assets/img/icon-128.png',
           to: path.join(__dirname, 'build'),
           force: true,
