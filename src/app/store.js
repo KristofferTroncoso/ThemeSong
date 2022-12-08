b9b39c5f16035efc98f51d00f64eb2a892b9ce7a
kristoffertroncoso <troncosokristoffer@gmail.com>
2022-12-08 13:15:38 -0500

    Disable unused feature stores

--- src/app/store.js
+++ src/app/store.js
@@ -1,22 +1,22 @@
 import create from 'zustand';
 import { immer } from 'zustand/middleware/immer';
 
-import { createUiOptionsSlice } from './UiOptions/uiOptionsSlice';
+// import { createUiOptionsSlice } from './UiOptions/uiOptionsSlice';
 import { createPopupSlice } from './popup/popupSlice';
 import { createSongSlice } from './Song/songSlice';
 import { createPlayerSlice } from './Player/playerSlice';
 import { createPaletteSlice } from './Palette/paletteSlice';
 import { createVisualizerSlice } from './Visualizer/visualizerSlice';
 import { createThemeSlice } from './Theme/themeSlice';
-import { createSnippetsSlice } from './Snippets/snippetsSlice';
+// import { createSnippetsSlice } from './Snippets/snippetsSlice';
 
 export const useStore = create(immer((...a) => ({
-  uiOptions: {...createUiOptionsSlice(...a)},
+  // uiOptions: {...createUiOptionsSlice(...a)},
   popup: {...createPopupSlice(...a)},
   song: {...createSongSlice(...a)},
   player: {...createPlayerSlice(...a)},
   palette: {...createPaletteSlice(...a)},
   visualizer: {...createVisualizerSlice(...a)},
   theme: {...createThemeSlice(...a)},
-  snippets: {...createSnippetsSlice(...a)}
+  // snippets: {...createSnippetsSlice(...a)}
 })))
