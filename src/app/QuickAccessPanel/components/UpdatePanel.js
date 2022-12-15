7b5080d91e362b6f9dbf5d064ba16e3089b76700
kristoffertroncoso <troncosokristoffer@gmail.com>
2022-12-14 19:42:47 -0500

    Update with new version and note

--- src/app/QuickAccessPanel/components/UpdatePanel.js
+++ src/app/QuickAccessPanel/components/UpdatePanel.js
@@ -13,8 +13,10 @@ function UpdatePanel() {
     <StyledPanelDiv css={css`background-color: #d95600;`}>
       <h2 css={css`padding: 0 2px; color: var(--themesong-primary-text-color);`}><u>ThemeSong Update / Notes</u></h2>
       <div css={css`color: var(--themesong-primary-text-color); padding: 2px; font-size: 13px; max-width: 280px;`}>
-        <p>v0.4.4</p>
-        <p>New Theme: "Ditto"!</p>
+        <p><b>v0.4.5 Visualizer Fix (Dec 15, 2022)</b></p>
+        <p>Visualizers finally animating for everyone? If they didn't before, please refresh and try again.</p>
+        <p>Thanks to the people who reviewed and helped track this bug down!</p>
+        <p><b>v0.4.4 New Theme: "Ditto"!</b></p>
         <p>YouTube Music Mobile and Apple Music clones.</p>
         <p>Click on the ThemeSong icon on your browser toolbar to check it out!</p>
       </div>
