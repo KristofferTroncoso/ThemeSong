3108c74256f5496522301d8ce4a4e39c6b5d376c
kristoffertroncoso <troncosokristoffer@gmail.com>
2023-01-24 16:03:42 -0500

    Clean up comments

--- src/app/QuickAccessPanel/components/PanelButton.js
+++ src/app/QuickAccessPanel/components/PanelButton.js
@@ -56,29 +56,4 @@ function PanelButton({color, bgColor, hoverColor, hoverBgColor, children, title,
   )
 }
 
-/* man... using ui component libraries really do just cause more headaches */
-
-// function PanelButton({children, ...props}) {
-//   return (
-//     <Button
-//       variant="contained" 
-//       disableElevation 
-//       css={css`
-//         height: 45px;
-//         min-width: 45px;
-//         width: 45px;
-//         margin: 5px;
-//         background: rgba(0,0,0,0.4);
-//         color: white;
-//         :hover {
-//           background-color: rgba(255, 255, 255, 0.4);
-//         }
-//       `}
-//       {...props}
-//     >
-//       {children}
-//     </Button>
-//   )
-// }
-
 export default PanelButton;
