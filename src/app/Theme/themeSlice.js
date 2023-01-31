4a86ca77c3ea43f43c9e93f1e587bb2b5cd7b828
kristoffertroncoso <troncosokristoffer@gmail.com>
2023-01-31 15:15:23 -0500

    Change themes data

--- src/app/Theme/themeSlice.js
+++ src/app/Theme/themeSlice.js
@@ -3,12 +3,16 @@ export const createThemeSlice = (set) => ({
   themes: [
     {
       themeId: "themeId:0",
+      id: "416034f2-bfb8-46e8-9929-5805dd59a688",
       dateAdded: 20220207,
       name: "Off",
       isDynamic: false,
+      hasLightMode: false,
+      hasDarkMode: true,
     },
     {
       themeId: "themeId:6",
+      id: "db8854e3-6753-4639-b244-c8091f3b9fcb",
       dateAdded: 20220602,
       name: "Dynamic",
       isDynamic: true,
@@ -16,6 +20,7 @@ export const createThemeSlice = (set) => ({
     },
     {
       themeId: "themeId:7",
+      id: "b458eaae-0cbd-4a44-8847-c7a6a6ea1be8",
       dateAdded: 20220702,
       name: "Static",
       isDynamic: false,
@@ -23,22 +28,26 @@ export const createThemeSlice = (set) => ({
     },
     // {
     //   themeId: "themeId:8",
+    //   id: "8383a680-c786-4c8d-82ee-59e0f1ea7c50",
     //   dateAdded: 20221014,
     //   name: "Custom",
     //   isDynamic: false,
     // },
     {
       themeId: "themeId:9",
+      id: "76dd54c5-78a2-4ca3-9c16-3d0d1aab367f",
       dateAdded: 20221111,
       name: "Ditto",
       isDynamic: false,
       variants: [
         {
           variantId: "variantId:1",
+          id: "55f83bbd-d794-49a8-8912-2b53af3f1d3f",
           name: "YouTube Music Mobile",
         },
         {
           variantId: "variantId:2",
+          id: "3f71704c-d344-4bd0-9013-a2da7bda13ef",
           name: "Apple Music",
         },
       ],
