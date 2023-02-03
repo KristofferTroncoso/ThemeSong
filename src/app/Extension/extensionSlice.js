f11a0a33678601761baef879159931586f69c19d
kristoffertroncoso <troncosokristoffer@gmail.com>
2023-02-02 20:28:11 -0500

    Add browser state

--- src/app/Extension/extensionSlice.js
+++ src/app/Extension/extensionSlice.js
@@ -1,9 +1,16 @@
 export const createExtensionSlice = (set) => ({
   showUpdateNote: false,
+  browser: "chrome",
   changeShowUpdateNote: (payload) => {
     console.log("extensionSlice: changeShowUpdateNote");
     set((state) => {
       state.extension.showUpdateNote = payload;
+    });
+  },
+  changeBrowser: (payload) => {
+    console.log("extensionSlice: changeBrowser");
+    set((state) => {
+      state.extension.browser = payload;
     });
   },
 });
