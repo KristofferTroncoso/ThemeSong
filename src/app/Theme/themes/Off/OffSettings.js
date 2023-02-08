c2882d3378e850070162b9cb753a462e87bd6ee4
kristoffertroncoso <troncosokristoffer@gmail.com>
2023-02-08 06:33:55 -0500

    Change Off theme title

--- src/app/Theme/themes/Off/OffSettings.js
+++ src/app/Theme/themes/Off/OffSettings.js
@@ -1,7 +1,7 @@
 export function OffSettings() {
   return (
     <div>
-      <h2>Active Theme: Off (YouTube Music™ default theme)</h2>
+      <h2>Active Theme: Off</h2>
       <p style={{ margin: "5px 0" }}>Now using YouTube Music™ default theme.</p>
       <p>Please choose a custom theme below.</p>
     </div>
