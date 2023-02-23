4f487139a4f06da9be86f0111a13e87091116b9e
kristoffertroncoso <troncosokristoffer@gmail.com>
2023-02-23 08:13:51 -0500

    Switch to react component

--- apps/themesong-extension/src/app/QuickAccessPanel/components/StyledPanelDiv.js
+++ apps/themesong-extension/src/app/QuickAccessPanel/components/StyledPanelDiv.js
@@ -1,11 +1,20 @@
-import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 
-const StyledPanelDiv = styled.div`
-  border: 1px solid rgba(255, 255, 255, 0.1);
-  border-radius: 8px;
-  background-color: rgba(0, 0, 0, 0.15);
-  padding: 6px 5px 3px;
-  margin-bottom: 6px;
-`;
+function StyledPanelDiv({ children, ...props }) {
+  return (
+    <div
+      css={css`
+        border: 1px solid rgba(255, 255, 255, 0.1);
+        border-radius: 8px;
+        background-color: rgba(0, 0, 0, 0.15);
+        padding: 6px 5px 3px;
+        margin-bottom: 6px;
+      `}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
 
 export default StyledPanelDiv;
