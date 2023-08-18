c4b88e0472e10e24d374a991391853c04190c41f
kristoffertroncoso <troncosokristoffer@gmail.com>
2023-08-18 02:38:39 -0400

    Add panel button to account for other themes

--- apps/extension/src/app/Theme/ThemePanel.js
+++ apps/extension/src/app/Theme/ThemePanel.js
@@ -4,11 +4,13 @@ import InvertColorsOffIcon from "@mui/icons-material/InvertColorsOff";
 import { GiAtom } from "react-icons/gi";
 import ColorizeIcon from "@mui/icons-material/Colorize";
 import AppleIcon from "@mui/icons-material/Apple";
+import PaletteIcon from "@mui/icons-material/Palette";
 
 import PanelButton from "../QuickAccessPanel/components/PanelButton";
 import { useStore } from "/src/app/store";
 
 function ThemePanel() {
+  const themes = useStore((state) => state.theme.themes);
   const activeTheme = useStore((state) => state.theme.activeTheme);
   const changeActiveTheme = useStore((state) => state.theme.changeActiveTheme);
 
@@ -127,6 +129,34 @@ function ThemePanel() {
             `}
           />
         </PanelButton>
+        {![
+          "416034f2-bfb8-46e8-9929-5805dd59a688",
+          "db8854e3-6753-4639-b244-c8091f3b9fcb",
+          "b458eaae-0cbd-4a44-8847-c7a6a6ea1be8",
+          "76dd54c5-78a2-4ca3-9c16-3d0d1aab367f",
+        ].includes(activeTheme) && (
+          <PanelButton
+            title={themes.find((theme) => theme.id === activeTheme).name}
+            bgColor="rgb(255 255 255 / 0.8)"
+            color="#207f96"
+            hoverColor="#207f96"
+            css={css`
+              height: 42px;
+              min-width: 45px;
+              width: 54px;
+              margin: 0 2px;
+              border: 0;
+              border-radius: 6px;
+              padding: 3px;
+            `}
+          >
+            <PaletteIcon
+              css={css`
+                font-size: 28px;
+              `}
+            />
+          </PanelButton>
+        )}
       </div>
     </div>
   );
