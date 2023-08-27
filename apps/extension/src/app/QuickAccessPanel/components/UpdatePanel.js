c57f56ac63abbb0df74a8f3120d3ae83f87a428c
kristoffertroncoso <troncosokristoffer@gmail.com>
2023-08-27 14:57:34 -0400

    Update version to v1.0.0

--- apps/extension/src/app/QuickAccessPanel/components/UpdatePanel.js
+++ apps/extension/src/app/QuickAccessPanel/components/UpdatePanel.js
@@ -31,11 +31,36 @@ function UpdatePanel() {
         `}
       >
         <p>
-          <b>v0.5.7</b>
-          <span> (August 8, 2023)</span>
+          <b>v1.0.0</b>
+          <span> (August 27, 2023)</span>
+          <span> 🎉</span>
         </p>
-        <p>- fix "End of Track" sleep feature</p>
-        <p>- better styling</p>
+        <br></br>
+        <p>
+          It's finally here! MIT OPEN-SOURCE. You're invited to contribute on{" "}
+          <a target="_blank" href="https://github.com/KristofferTroncoso/ThemeSong">
+            GitHub
+          </a>
+          !
+        </p>
+        <br></br>
+        <h4>Changes:</h4>
+        <p>
+          - <b>Notifications</b>: Toggle desktop notifications for song changes.
+        </p>
+        <p>
+          - <b>Pieces</b>: Toggle on small UI options such as hiding the Cast icon.
+        </p>
+        <br></br>
+        <p>
+          <b>
+            <i>Thanks to the first 2000 users! I've seen all the stars and reviews! You guys are great!</i>
+          </b>
+        </p>
+        <p>
+          <i>-- Kris / GraphiteEater</i>
+          <span style={{ fontSize: "18px" }}> 😃</span>
+        </p>
       </div>
       <button
         css={css`
@@ -60,6 +85,33 @@ function UpdatePanel() {
       >
         OK
       </button>
+      <a
+        href={STORE_URL}
+        target="_blank"
+        rel="noreferrer"
+        css={{
+          textDecoration: "none",
+          color: "white",
+        }}
+      >
+        <button
+          css={css`
+            margin: 5px 8px 0 15px;
+            width: 100px;
+            padding: 3px;
+            background-color: dodgerblue;
+            border: 1px solid black;
+            border-radius: 3px;
+            color: #000;
+
+            :hover {
+              background-color: yellow;
+            }
+          `}
+        >
+          ⭐️ Leave a rating
+        </button>
+      </a>
     </StyledPanelDiv>
   );
 }
