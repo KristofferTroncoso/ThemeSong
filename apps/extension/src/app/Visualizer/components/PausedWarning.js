4370d5793cdfb076abeeea10c4b577d0e90dfd26
kristoffertroncoso <troncosokristoffer@gmail.com>
2023-09-17 14:38:59 -0400

    Remove comments

--- apps/extension/src/app/Visualizer/components/PausedWarning.js
+++ apps/extension/src/app/Visualizer/components/PausedWarning.js
@@ -5,35 +5,6 @@ import PauseIcon from "@mui/icons-material/Pause";
 function PausedWarning() {
   const isSongPlaying = useStore((state) => state.player.isSongPlaying);
 
-  // return (
-  //   <div id="PausedWarning">
-  //     {!isSongPlaying && (
-  //       <div
-  //         css={css`
-  //           margin: 10px;
-  //           padding: 10px;
-  //           background-color: rgb(0 0 0 / 0.7);
-  //           color: #eee;
-  //           border: 4px solid #777;
-  //           border-radius: 10px;
-  //           position: absolute;
-  //           z-index: 1000;
-  //           top: 0;
-  //           left: 0;
-  //         `}
-  //       >
-  //         <PauseIcon
-  //           css={css`
-  //             height: 20%;
-  //             width: 20%;
-  //             font-size: height / 4;
-  //           `}
-  //         />
-  //       </div>
-  //     )}
-  //   </div>
-  // );
-
   return (
     <div id="PausedWarning">
       {!isSongPlaying && (
