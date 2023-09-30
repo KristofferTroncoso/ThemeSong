39adbc8814dd838c2646dda43e65668e77331490
kristoffertroncoso <troncosokristoffer@gmail.com>
2023-09-30 15:56:56 -0400

    Transfer icons from store data to component level

--- apps/extension/src/app/Visualizer/VisualizerPanel.js
+++ apps/extension/src/app/Visualizer/VisualizerPanel.js
@@ -5,6 +5,14 @@ import StyledPanelDiv from "../QuickAccessPanel/components/StyledPanelDiv";
 import PanelButton from "../QuickAccessPanel/components/PanelButton";
 import { GiProtectionGlasses } from "react-icons/gi";
 
+import { TbWaveSawTool } from "react-icons/tb";
+import { ImStatsBars } from "react-icons/im";
+import { RiBubbleChartFill } from "react-icons/ri";
+import { AiOutlineExperiment } from "react-icons/ai";
+import { GiSpiderMask } from "react-icons/gi";
+import { RxGlobe } from "react-icons/rx";
+import { BsSnow2, BsFillCassetteFill } from "react-icons/bs";
+
 function VisualizerVariantButton({ color, bgColor, hoverColor, hoverBgColor, children, title, ...props }) {
   return (
     <button
@@ -17,7 +25,7 @@ function VisualizerVariantButton({ color, bgColor, hoverColor, hoverBgColor, chi
         color: ${color || "#fff"};
         border: 0;
         border-radius: 4px;
-        transition: background 0.2s ease-in-out;
+        transition: background-color 0.2s ease-in-out;
         display: flex;
         justify-content: center;
         align-items: center;
@@ -99,7 +107,18 @@ function VisualizerPanel() {
               onClick={(e) => handleVisualizerButtonClick(visualizer.id)}
               key={visualizer.id}
             >
-              {visualizer.icon}
+              {
+                {
+                  "6aa34dd4-6775-46c1-8dbb-7ac2931ff80d": <TbWaveSawTool />,
+                  "51dc50c8-eb06-4086-ad9c-a89758f63db6": <ImStatsBars />,
+                  "685d0ec7-5c52-4e48-a43d-11184a39f3da": <RiBubbleChartFill />,
+                  "8315ac5f-0de5-4ef1-ac5d-a4bc6d7b21ae": <AiOutlineExperiment />,
+                  "57e7f170-a53d-4207-87f0-67633df37959": <GiSpiderMask />,
+                  "2f34a5b3-6d29-42c8-bac0-a7356ee88151": <RxGlobe />,
+                  "86a81510-3e5d-4d1e-9318-3ea0750393a3": <BsSnow2 />,
+                  "f8cfcb9f-6639-4702-aa44-2261ba7a543b": <BsFillCassetteFill />,
+                }[visualizer.id]
+              }
             </PanelButton>
           ))}
         </div>
--- apps/extension/src/app/Visualizer/visualizerSlice.js
+++ apps/extension/src/app/Visualizer/visualizerSlice.js
@@ -1,23 +1,13 @@
-import { TbWaveSawTool } from "react-icons/tb";
-import { ImStatsBars } from "react-icons/im";
-import { RiBubbleChartFill } from "react-icons/ri";
-import { AiOutlineExperiment } from "react-icons/ai";
-import { GiSpiderMask } from "react-icons/gi";
-import { RxGlobe } from "react-icons/rx";
-import { BsSnow2, BsFillCassetteFill } from "react-icons/bs";
-
 export const createVisualizerSlice = (set, get) => ({
   isVisualizerOn: false,
   visualizers: [
     {
       id: "6aa34dd4-6775-46c1-8dbb-7ac2931ff80d",
       name: "Wavy",
-      icon: <TbWaveSawTool />,
     },
     {
       id: "51dc50c8-eb06-4086-ad9c-a89758f63db6",
       name: "Bars",
-      icon: <ImStatsBars />,
       variants: [
         {
           id: "12bfd49e-47a2-4cc8-90a9-3e669f7a0c78",
@@ -40,7 +30,6 @@ export const createVisualizerSlice = (set, get) => ({
     {
       id: "685d0ec7-5c52-4e48-a43d-11184a39f3da",
       name: "Circles",
-      icon: <RiBubbleChartFill />,
       variants: [
         {
           id: "2040b849-8c7c-4290-8ff8-c0d7716cca77",
@@ -75,27 +64,22 @@ export const createVisualizerSlice = (set, get) => ({
     {
       id: "8315ac5f-0de5-4ef1-ac5d-a4bc6d7b21ae",
       name: "Experimental",
-      icon: <AiOutlineExperiment />,
     },
     {
       id: "57e7f170-a53d-4207-87f0-67633df37959",
       name: "Spider-Verse",
-      icon: <GiSpiderMask />,
     },
     {
       id: "2f34a5b3-6d29-42c8-bac0-a7356ee88151",
       name: "Disco Ball",
-      icon: <RxGlobe />,
     },
     {
       id: "86a81510-3e5d-4d1e-9318-3ea0750393a3",
       name: "Snowfall",
-      icon: <BsSnow2 />,
     },
     {
       id: "f8cfcb9f-6639-4702-aa44-2261ba7a543b",
       name: "RetroBars",
-      icon: <BsFillCassetteFill />,
     },
   ],
   prefs: {
