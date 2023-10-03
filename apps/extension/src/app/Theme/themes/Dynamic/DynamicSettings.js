eee579f789cb7205b426730722d333f05c0cdeea
kristoffertroncoso <troncosokristoffer@gmail.com>
2023-10-03 16:29:13 -0400

    Change dropdown form to icons

--- apps/extension/src/app/Theme/themes/Dynamic/DynamicSettings.js
+++ apps/extension/src/app/Theme/themes/Dynamic/DynamicSettings.js
@@ -3,14 +3,19 @@ import DynamicDarkSettings from "./Dark/DynamicDarkSettings";
 import DynamicLightSettings from "./Light/DynamicLightSettings";
 import { useStore } from "/src/app/store";
 
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import { CgDarkMode } from "react-icons/cg";
+import PanelButton from "../../../QuickAccessPanel/components/PanelButton";
+
 function DynamicSettings() {
   const setSingleThemePrefs = useStore((state) => state.theme.setSingleThemePrefs);
   const dynamicUserPrefs = useStore((state) => state.theme.prefs["db8854e3-6753-4639-b244-c8091f3b9fcb"]);
 
-  function handleDarkLightChange(e) {
+  function handleDarkLightChange(value) {
     let newDynamicUserPrefs = {
       ...dynamicUserPrefs,
-      [e.target.name]: e.target.value,
+      appearance: value,
     };
     setSingleThemePrefs("db8854e3-6753-4639-b244-c8091f3b9fcb", newDynamicUserPrefs);
   }
@@ -67,37 +72,81 @@ function DynamicSettings() {
         Adjust brightness and saturation below.
       </p>
       <form
-        onSubmit={(e) => e.preventDefault()}
+        name="appearance"
         css={css`
-          margin-bottom: 8px;
-          text-align: right;
+          display: flex;
+          justify-content: right;
+          align-items: center;
         `}
+        onSubmit={(e) => e.preventDefault()}
       >
-        <label
-          htmlFor="appearance"
-          css={css`
-            margin-right: 8px;
-          `}
-        >
-          Appearance:
-        </label>
-        <select
-          id="appearance"
+        <PanelButton
+          title="Dark Mode"
           name="appearance"
-          value={dynamicUserPrefs.appearance}
-          onChange={handleDarkLightChange}
           css={css`
-            background-color: #333;
-            color: white;
-            border: 1px solid #888;
-            border-radius: 4px;
-            outline: 0;
+            height: 25px;
+            width: 35px;
+            margin: 0 2px;
+            background: rgb(255 255 255 / 0.2);
+            background: ${dynamicUserPrefs.appearance === "dark" && "rgb(255 255 255 / 0.8)"};
+            color: ${dynamicUserPrefs.appearance === "dark" && "#9d00ff"};
+            border: 0;
+            border-radius: 3px;
+            :hover {
+              color: #9d00ff;
+            }
           `}
+          value="dark"
+          onClick={(e) => handleDarkLightChange("dark")}
         >
-          <option value="light">Light</option>
-          <option value="dark">Dark</option>
-          <option value="system">Use Device Setting</option>
-        </select>
+          <DarkModeIcon style={{ fontSize: 17 }} />
+        </PanelButton>
+        <PanelButton
+          title="Light Mode"
+          name="appearance"
+          css={css`
+            height: 25px;
+            width: 35px;
+            margin: 0 2px;
+            background: rgb(255 255 255 / 0.2);
+            background: ${dynamicUserPrefs.appearance === "light" && "rgb(255 255 255 / 0.8)"};
+            color: ${dynamicUserPrefs.appearance === "light" && "#de9800"};
+            border: 0;
+            border-radius: 3px;
+            :hover {
+              color: #fcad00;
+            }
+          `}
+          value="light"
+          onClick={(e) => handleDarkLightChange("light")}
+        >
+          <LightModeIcon
+            css={css`
+              font-size: 17px;
+            `}
+          />
+        </PanelButton>
+        <PanelButton
+          title="Use Device Setting"
+          name="appearance"
+          css={css`
+            height: 25px;
+            width: 35px;
+            margin: 0 2px;
+            background: rgb(255 255 255 / 0.2);
+            background: ${dynamicUserPrefs.appearance === "system" && "rgb(255 255 255 / 0.8)"};
+            color: ${dynamicUserPrefs.appearance === "system" && "#009961"};
+            border: 0;
+            border-radius: 3px;
+            :hover {
+              color: #009961;
+            }
+          `}
+          value="system"
+          onClick={(e) => handleDarkLightChange("system")}
+        >
+          <CgDarkMode size={18} />
+        </PanelButton>
       </form>
       <div
         css={css`
