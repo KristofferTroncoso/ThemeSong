9a8673ee6c69c89a60f675475f388fe0b91ef0cf
kristoffertroncoso <troncosokristoffer@gmail.com>
2023-12-13 10:12:04 -0800

    Lighten gradient

--- apps/extension/src/app/Theme/themes/YouTubeMusicMobile/YouTubeMusicMobile.js
+++ apps/extension/src/app/Theme/themes/YouTubeMusicMobile/YouTubeMusicMobile.js
@@ -14,7 +14,7 @@ function YouTubeMusicMobile() {
 
   useEffect(() => {
     if (playerUiState === "PLAYER_PAGE_OPEN") {
-      menubar.content = `oklch(0.3 ${dominantColorLCH[1] / 3} ${dominantColorLCH[2]})`;
+      menubar.content = `oklch(0.35 ${dominantColorLCH[1] / 3} ${dominantColorLCH[2]})`;
     } else {
       menubar.content = "#0d0d0d";
     }
@@ -33,8 +33,8 @@ function YouTubeMusicMobile() {
 
         :root  {
           --ts-navbar-color: #0d0d0d;
-          --ts-playerpage-color: linear-gradient(to bottom, oklch(0.3 calc(var(--ts-palette-dominant-c) / 3) var(--ts-palette-dominant-h)), oklch(0.25 calc(var(--ts-palette-dominant-c) / 5) var(--ts-palette-dominant-h)));
-          --ts-playerpageavtoggle-color: oklch(0.35 calc(var(--ts-palette-dominant-c) / 3) var(--ts-palette-dominant-h));
+          --ts-playerpage-color: linear-gradient(to bottom, oklch(0.35 calc(var(--ts-palette-dominant-c) / 3) var(--ts-palette-dominant-h)), oklch(0.25 calc(var(--ts-palette-dominant-c) / 5) var(--ts-palette-dominant-h)));
+          --ts-playerpageavtoggle-color: oklch(0.4 calc(var(--ts-palette-dominant-c) / 2) var(--ts-palette-dominant-h));
           --ts-playerbar-color: #1a1a1a;
           --ts-body-color: #000000;
           --ts-body-alpha-gradient-color: rgb(0 0 0 / 0.7);
@@ -45,8 +45,8 @@ function YouTubeMusicMobile() {
       {(playerUiState === "PLAYER_PAGE_OPEN" || playerUiState === "FULLSCREEN") &&
         /* css */ ` 
         :root  {
-          --ts-navbar-color: oklch(0.3 calc(var(--ts-palette-dominant-c) / 3) var(--ts-palette-dominant-h));
-          --ts-sidebar-color: linear-gradient(to bottom, oklch(0.3 calc(var(--ts-palette-dominant-c) / 3) var(--ts-palette-dominant-h)), oklch(0.25 calc(var(--ts-palette-dominant-c) / 5) var(--ts-palette-dominant-h)));
+          --ts-navbar-color: oklch(0.35 calc(var(--ts-palette-dominant-c) / 3) var(--ts-palette-dominant-h));
+          --ts-sidebar-color: linear-gradient(to bottom, oklch(0.35 calc(var(--ts-palette-dominant-c) / 3) var(--ts-palette-dominant-h)), oklch(0.25 calc(var(--ts-palette-dominant-c) / 5) var(--ts-palette-dominant-h)));
           --ts-playerbar-color: oklch(0.25 calc(var(--ts-palette-dominant-c) / 5) var(--ts-palette-dominant-h));
         }
       `}
