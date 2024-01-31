e19455754420b3f9461c3762ca8ca5a45c2bfb70
KristofferTroncoso <troncosokristoffer@gmail.com>
2024-01-30 20:16:59 -0500

    Style scrollbars

--- apps/extension/src/app/Theme/css/core/scrollbars.js
+++ apps/extension/src/app/Theme/css/core/scrollbars.js
@@ -9,7 +9,22 @@ html {
   scrollbar-color: var(--ts-base-100-alpha-02-color) transparent;
 }
 
+/* carousel on homepage and stuff */
 #items.ytmusic-carousel {
   scrollbar-width: none;
+}
+
+/* pills and chips */
+#chips.ytmusic-chip-cloud-renderer {
+  scrollbar-width: none;
+}
+
+/* sidebar playlist list */
+#items.ytmusic-guide-section-renderer {
+  scrollbar-width: none;
+}
+
+#items.ytmusic-guide-section-renderer:hover {
+  scrollbar-width: auto;
 }
 `;
