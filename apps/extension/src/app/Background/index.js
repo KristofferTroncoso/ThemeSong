b7301e095c5eed15ff81a23f37ed9eef860e3994
KristofferTroncoso <troncosokristoffer@gmail.com>
2024-03-01 15:01:05 -0500

    Add next song feature for notification

--- apps/extension/src/app/Background/index.js
+++ apps/extension/src/app/Background/index.js
@@ -3,3 +3,10 @@ import handleOnMessage from "./handleOnMessage";
 
 chrome.runtime.onInstalled.addListener(handleOnInstalled);
 chrome.runtime.onMessage.addListener(handleOnMessage);
+chrome.notifications.onButtonClicked.addListener((notificationId, buttonIndex) => {
+  chrome.tabs.query({}, (tabs) => {
+    let ytmTabs = tabs.filter((tab) => "url" in tab);
+    console.log(ytmTabs);
+    chrome.tabs.sendMessage(ytmTabs[0].id, { message: "next-button" });
+  });
+});
