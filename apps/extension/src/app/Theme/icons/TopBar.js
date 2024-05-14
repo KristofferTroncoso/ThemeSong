9ec83d025eded6d22eb1266d7f4865abf76c0a8d
kristoffertroncoso <troncosokristoffer@gmail.com>
2024-05-14 09:58:50 -0400

    Update icon to include sidebar

--- apps/extension/src/app/Theme/icons/TopBar.js
+++ apps/extension/src/app/Theme/icons/TopBar.js
@@ -1,9 +1,14 @@
 function TopBar({ ...props }) {
   return (
-    <svg width="100%" length="100%" viewBox="0 0 48 42" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <rect x="2.5" y="2.5" width="43" height="37" rx="9.5" stroke="currentColor" strokeWidth="5" />
-      <rect x="2.5" y="5.5" width="43" height="34" rx="9.5" stroke="currentColor" strokeWidth="5" />
-      <line x1="6" y1="10" x2="42" y2="10" stroke="currentColor" strokeWidth="6" strokeLinecap="square" />
+    <svg fill="none" viewBox="0 0 48 42" width="100%" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <g stroke="currentColor">
+        <g strokeWidth="5">
+          <rect height="37" rx="9.5" width="43" x="2.5" y="2.5" />
+          <rect height="34" rx="9.5" width="43" x="2.5" y="5.5" />
+          <path d="m5.5 10.5h37" strokeLinecap="square" />
+        </g>
+        <path d="m10 15-.00001 19" strokeLinecap="square" strokeWidth="10" />
+      </g>
     </svg>
   );
 }
